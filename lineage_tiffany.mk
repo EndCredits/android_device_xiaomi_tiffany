fd514add65160446cbcef237ba34c52acb5d34de
EndCredits <alicization.han@gmail.com>
2021-06-07 11:52:43 +0800

    Revert "treewide: Drop Lineage Stuffs and dot ify"
    This reverts commit 3c92efd0cd91f8f7995a23609a7440abbc0d42d1.

--- lineage_tiffany.mk
+++ lineage_tiffany.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/tiffany/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tiffany
-PRODUCT_NAME := dot_tiffany
+PRODUCT_NAME := lineage_tiffany
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi Mi 5X
 PRODUCT_MANUFACTURER := Xiaomi
